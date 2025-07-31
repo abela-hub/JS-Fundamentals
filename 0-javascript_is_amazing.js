ba28248eb274f14fc8c134a2d6ac8cce424d4de4
abela-hub <derejeabel986@gmail.com>
2025-07-31 05:24:35 -0400

    my java script project

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+// 0-javascript_is_amazing.js
+
+// Create a constant variable named myVar and assign the required string to it.
+const myVar = "JavaScript is amazing";
+
+// Use console.log() to print the value of myVar to the console.
+console.log(myVar);
